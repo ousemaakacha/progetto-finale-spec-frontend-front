6abcb3871aca5d707d4099555331475f10561a4d
ousemaakacha <akachaousema@gmail.com>
2026-07-12 12:02:24 +0200

    feat: add route for homepage to app.jsx

--- food-compare/src/App.jsx
+++ food-compare/src/App.jsx
@@ -1,5 +1,6 @@
-// import { Routes, Route } from "react-router-dom";
-// import FavoritesPage from "./pages/FavoritesPage.jsx";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import FavoritesPage from "./pages/FavoritesPage.jsx";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Routes>
+      <Route path="/" element={<HomePage />} />
       {/* <Route path="/favorites" element={<FavoritesPage />}/> */}
     </Routes>
     </>
