34e2f172c360a3b338dc08db9fdb0723517399df
ousemaakacha <akachaousema@gmail.com>
2026-07-12 14:28:29 +0200

    fix: styling for not found page

--- food-compare/src/main.jsx
+++ food-compare/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { FavoritesProvider } from './context/FavoritesContext.jsx';
+import { CompareProvider } from './context/CompareContext.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./style.css";
 import App from './App.jsx'
-import { FavoritesProvider } from './context/FavoritesContext.jsx';
-import { CompareProvider } from './context/CompareContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- food-compare/src/pages/NotFoundPage.jsx
+++ food-compare/src/pages/NotFoundPage.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function NotFoundPage(){
     return (
         <div className="text-center py-5">
-            <h1>404</h1>
-            <p>Pagina non trovata</p>
-            <Link to="/">Torna alla Home</Link>
+            <h1 className="display-4">404</h1>
+            <p className="lead text-muted">Pagina non trovata</p>
+            <Link className="btn btn-success" to="/">Torna alla Home</Link>
         </div>
     );
 }
